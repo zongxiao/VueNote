0479368ec9c60371f276db777ed354502ccba45e
罗宗校 <905616398@qq.com>
2021-10-29 10:36:52 +0800

    add

--- mycode/vue_test/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import {allMixin} from './mixin'
-Vue.config.productionTip = false;
-Vue.mixin(allMixin)
-new Vue({
-  render: h => h(App)
-}).$mount("#app")
