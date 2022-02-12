72cf71edb884ede30e822dd06fe21be892977aa7
mac m1 <905616398@qq.com>
2022-02-12 23:45:19 +0800

    add

--- project/myblog/src/components/HomeBanner.vue
+++ project/myblog/src/components/HomeBanner.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="banner">
-    <img src="../assets/img/banner/banner.png" alt=""/>
+  <div class="banner swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="../assets/img/banner/banner.png" alt=""/>
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/img/banner/banner.png" alt=""/>
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/img/banner/banner.png" alt=""/>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
 export default {
   name: "Banner",
+  swiper: null,
+  mounted() {
+    this.swiper = new Swiper('.swiper-container', {
+      speed: 3000,
+      autoplay: 3000
+    })
+  }
 };
 </script>
 
